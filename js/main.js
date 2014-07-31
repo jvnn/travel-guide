def8a4b53118f4a3c462bdaa1e30e54435bb5074
Jussi Nieminen <nieminen.jussi@gmail.com>
2014-07-31 20:53:15 +0200

    custom markers for locations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var guiService = require('GuiService.js');
 var map = null;
 var currentPopupData = null;
 
+window.onresize = function() {
+	document.getElementById('map').style.height = window.innerHeight - 20 + "px";
+}
+
 locService.readData('json/data.json', initMap);
 
 function initMap() {
@@ -39,6 +43,10 @@ function createMarkers() {
 
 function createMarker(item) {
 	var loc = locService.getLocation(item);
+	var tags = item.tags;
+	if (tags == null) {
+		tags = [];
+	}
 	if (loc) {
 		var marker = null;
 		if (loc.radius) {
@@ -50,7 +58,8 @@ function createMarker(item) {
 				fillOpacity: 0.1
 			}).addTo(map);
 		} else {
-			marker = L.marker([loc.lat, loc.lon]).addTo(map);
+			var icon = getMarkerIcon(tags);
+			marker = L.marker([loc.lat, loc.lon], {icon: icon}).addTo(map);
 		}
 
 		if (marker) {
@@ -77,4 +86,81 @@ function addNewItem() {
 		createMarker(newItem);
 	}
 	map.closePopup(currentPopupData.popup);
+}
+
+
+var markerAccommodation = L.icon({
+		iconUrl: '../img/marker-accommodation.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerNature = L.icon({
+		iconUrl: '../img/marker-nature.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerCity = L.icon({
+		iconUrl: '../img/marker-city.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerTravel = L.icon({
+		iconUrl: '../img/marker-travel.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerSight = L.icon({
+		iconUrl: '../img/marker-sight.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerDefault = L.icon({
+		iconUrl: '../img/marker-default.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+var markerWaypoint = L.icon({
+		iconUrl: '../img/marker-waypoint.png',
+		iconSize: [40, 49],
+		iconAnchor: [15, 49],
+		popupAnchor: [5, -45]
+});
+
+function containsSome(array, list) {
+	for (var i = 0; i < list.length; i++) {
+		if (array.indexOf(list[i]) >= 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getMarkerIcon(tags) {
+	var icon = markerDefault;
+	if (containsSome(tags, ["accommodation", "hotel", "motel", "hostel"])) {
+		icon = markerAccommodation;
+	} else if (containsSome(tags, ["lookout", "nature", "hiking", "national park"])) {
+		icon = markerNature;
+	} else if (containsSome(tags, ["city", "town", "location"])) {
+		icon = markerCity;
+	} else if (containsSome(tags, ["monument", "tourist attraction", "attraction", "sight", "tour"])) {
+		icon = markerSight;
+	} else if (containsSome(tags, ["travel", "airport"])) {
+		icon = markerTravel;
+	} else if (containsSome(tags, ["waypoint", "route", "target"])) {
+		icon = markerWaypoint;
+	}
+	return icon;
 }
